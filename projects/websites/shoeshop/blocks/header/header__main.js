8e9c6ca6fac35b847ef32d78976c953eeb923b24
unknown <nikhaienko@bk.ru>
2020-01-21 18:54:28 +0700

    Shoeshop Для блока menu добавлены разметка, стили, функциии в скрипт
    Для блока menu (основное меню, расположенное в шапке сайта) добавлены разметка для мобильных телефонов и планшетов, а также дополнительные стили для неё.
    
    Кроме этого в скрипт добавлены функции для открытия/закрытия вкладок меню (openSubMenu и closeSubMenu), а также добавлены переменные, которые участвуют в данных функциях

--- projects/websites/shoeshop/blocks/header/header__main.js
+++ projects/websites/shoeshop/blocks/header/header__main.js
@@ -1,10 +1,21 @@
-//Код для блока header
-//Инициализация переменных
-let menuMain = document.querySelector(".header__menu-main");
+//Инициализация переменных для блока кода, отвечающего за скрытие основного меню
+//при кликании за пределами блока либо на кнопку-бургер
+let menuMain = document.querySelector(".header__menu-main");//Родительский блок для всего меню
 let menuBurg = document.querySelector(".burg-menu");//Кнопка с эмблемой меню
 //let circle = document.querySelectorAll(".pag .circle");//Создание массива из элементов управления слайдером
 
-//Выполняется проверка на наличие указанного класса у элемента.
+//Объявление переменных для блока кода, ответственного за открытие/закрытие вкладок под-меню
+let menuMainItems = document.querySelectorAll(".mobile .item-nav");//Элементы основного меню
+let itemSubNav = document.querySelectorAll(".mobile .item-subnav");//Элементы вкладки "New"
+let menuSub = document.querySelectorAll(".mobile .list--hid");//Меню для вкладок "New...Brands"
+let catalog = document.querySelectorAll(".mobile .catalog-mobile");//Меню для вкладок "New for men..."
+let itemCloseMenu = document.querySelectorAll(".mobile .item--pe__prev");//Элементы, которые закрывают вкладки
+//Объявление функций, которые открывают/закрывают вкладки под-меню
+openSubMenu();
+closeSubMenu();
+
+//Код для закрытия основного меню при нажатии за пределами блока меню либо на кнопку с эмблемой меню
+//Функция, которая выполняет проверку на наличие указанного класса у элемента.
 //Если этого класса нет, то он добавляется к элементу
 let toggleMenu = function toggleMenu() {
   menuMain.classList.toggle("active");
@@ -12,7 +23,7 @@ let toggleMenu = function toggleMenu() {
 
 //На кнопку с эмблемой меню устанавливается обработчик события
 menuBurg.addEventListener("click", function(e) {
-  //Для отображения/ скрытия меню
+  //Для отображения/ скрытия основного меню
   e.stopPropagation();
 
   toggleMenu();
@@ -39,6 +50,48 @@ document.addEventListener("click", function(e) {
     toggleMenu();
   };
 });
+
+//Код для открытия/закрытия вкладок под-меню
+//Функция, которая открывает под-меню при нажатии на соответствующие элементы основного меню
+function openSubMenu() {
+  //Открываются меню для вкладок "New...Brands" при нажатии на соответствующий элемент из основного меню
+  for (let i = 0; i < menuMainItems.length; i++) {
+    menuMainItems[i].addEventListener ("click", function () {
+      menuSub[i].style.transform = "translateX(0)";
+    });
+  };
+
+  for (let i = 0; i < itemSubNav.length; i++) {
+    //Открываются вкладки для под-меню "New" при кликании на его элементы
+    itemSubNav[i].addEventListener ("click", function () {
+      catalog[i].style.transform = "translateX(0)";
+    });
+  };
+};
+
+//Функция, которая закрывает вкладки при нажатии на соответствующие элементы основного меню/под-меню
+function closeSubMenu() {
+  for (let i = 0; i < itemCloseMenu.length; i++) {
+    if (i < 1) {//Т.к. первый элемент должен закрывать саму вкладку "New"
+      itemCloseMenu[i].addEventListener ("click", function () {
+        menuSub[i].style.transform = "translateX(100%)";
+      });
+    } else if (i > 3) {//Т.к. начиная с 4 элемента массива происходит закрытие вкладок "Men...Brands"
+      itemCloseMenu[i].addEventListener ("click", function () {
+        for (let k = 1; k < menuSub.length; k++) {
+          menuSub[k].style.transform = "translateX(100%)";
+        };
+      });
+    } else {//Закрывает элементы под-меню, расположенные на вкладке "New"
+      itemCloseMenu[i].addEventListener ("click", function () {
+        //Цикл перебирает элементы с классом "Сatalog" (new for men, new for women, new for kids)
+        for (let k = 0; k < catalog.length; k++) {
+          catalog[k].style.transform = "translateX(100%)";
+        };
+      });
+    };
+  };
+};
 
 
 
