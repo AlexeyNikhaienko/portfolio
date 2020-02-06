9b865e7dd67de2460e4dd0b02e02ca31f0bf03df
Alexej <nikhaienko@bk.ru>
2020-02-06 10:39:36 +0700

    Shoeshop В блоке "testimonial" изменил скрипт для сдайдера

--- projects/websites/shoeshop/blocks/content/_testimonial/testimonial__main.js
+++ projects/websites/shoeshop/blocks/content/_testimonial/testimonial__main.js
@@ -3,74 +3,60 @@ const testimonialSlide = document.querySelector(".testimonial");
 const quotesArray = document.querySelectorAll(".testimonial .quote");
 const prevBtnQuote = document.querySelector(".testimonial .arrow--left");
 const nextBtnQuote = document.querySelector(".testimonial .arrow--right");
+//Переменная, которая будет хранить значения текущего индекса слайда
+let curIndexSlide = 0;
 
 //Объявление переменных, котрые участвуют в работе автоматического показа слайдов
 const intervalTime = 4000;//Через сколько времени (мс) показывается новый слайд
-const autoShowQuotes = true; //Автозапуск слайдера
 let quoteInterval;//Переменная-таймер
 
-//Функции для добавления/удалени класса у элемента
-function nextQuoteSlide() {
-  //Объявление переменной с искомым классом
-  const currentQuote = document.querySelector(".slider__quote");
-  currentQuote.classList.remove("slider__quote");
-
-  //Если следующий элемент не последний, то ему добавляется класс
-  //В противном случае класс добавляется первому элементу
-  if (currentQuote.nextElementSibling) {
-    currentQuote.nextElementSibling.classList.add("slider__quote");
-  } else {
-    quotesArray[0].classList.add("slider__quote");
+//Определение функции, которая добавляет/удаляет класс у элемента
+function showQuoteSlide() {
+  for (let i = 0; i < quotesArray.length; i++) {
+    quotesArray[i].classList.remove("slider__quote");
   }
-
-  //Удаление класса у элемента после проверки
-  setTimeout(function() {
-    currentQuote.classList.remove("slider__quote");
-  });
+  quotesArray[curIndexSlide].classList.add("slider__quote");
 }
 
+//Вызов функции. Выведет сразу первый слайд при загрузке страницы
+showQuoteSlide();
+
+//Функции для показа предыдущего/следующего слайда
 function prevQuoteSlide() {
-  const currentQuote = document.querySelector(".slider__quote");
-  currentQuote.classList.remove("slider__quote");
-
-  //Если предыдующий элемент не первый, то ему добавляется класс
-  //В противном случае класс добавляется последнему элементу
-  if (currentQuote.previousElementSibling) {
-    currentQuote.previousElementSibling.classList.add("slider__quote");
+  if (curIndexSlide == 0) {
+    curIndexSlide = quotesArray.length - 1;
   } else {
-    quotesArray[quotesArray.length - 1].classList.add("slider__quote");
+    curIndexSlide--;
   }
+  showQuoteSlide();
+}
 
-  setTimeout(function() {
-    currentQuote.classList.remove("slider__quote");
-  });
+function nextQuoteSlide() {
+  if (curIndexSlide == quotesArray.length - 1) {
+    curIndexSlide = 0;
+  } else {
+    curIndexSlide++;
+  }
+  showQuoteSlide();
 }
 
 //Установка слушателей на кнопки навигации
 //Работают только по клику мыши
 prevBtnQuote.addEventListener("click", function() {
   prevQuoteSlide();
-
-  if (autoShowQuotes) {
-    //Сброс таймера при клике на кнопку
-    clearInterval(quoteInterval);
-    quoteInterval = setInterval(nextQuoteSlide, intervalTime);
-  }
+  //Сброс таймера для корректной работы автослайдера после клика
+  clearInterval(quoteInterval);
+  quoteInterval = setInterval(nextQuoteSlide, intervalTime);
 });
 
 nextBtnQuote.addEventListener("click", function() {
   nextQuoteSlide();
-
-  if (autoShowQuotes) {
-    clearInterval(quoteInterval);
-    quoteInterval = setInterval(nextQuoteSlide, intervalTime);
-  }
+  clearInterval(quoteInterval);
+  quoteInterval = setInterval(nextQuoteSlide, intervalTime);
 });
 
-//Если autoShowQuotes = true, то выполняется автозапуск с показом слайдов ч/з заданныйы интевал
-if (autoShowQuotes) {
-  quoteInterval = setInterval(nextQuoteSlide, intervalTime);
-}
+//Автослайдер с показом слайдов ч/з заданныйы интевал
+quoteInterval = setInterval(nextQuoteSlide, intervalTime);
 
 //Слайдер для touch-событий
 const touchSlider = function(element) {
