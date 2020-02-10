6e628bfae77fa76beaf48cb106369c38a5447278
Alexej <nikhaienko@bk.ru>
2020-02-10 19:08:41 +0700

    Shoeshop Для блока "blog" добавлен touch-слайдер

--- projects/websites/shoeshop/blocks/content/_blog/blog__main.js
+++ projects/websites/shoeshop/blocks/content/_blog/blog__main.js
@@ -1,5 +1,5 @@
 //Объявление переменных
-const blogSlide = document.querySelectorAll(".blog");
+const blogSlide = document.querySelector(".slider-blog");
 const blogSlideArray = document.querySelectorAll(".blog .pic");
 const prevBtnBlog = document.querySelector(".control .left");
 const nextBtnBlog = document.querySelector(".control .right");
@@ -62,4 +62,73 @@ nextBtnBlog.addEventListener("click", function() {
 });
 
 //Автослайдер
-blogInterval = setInterval(nextBlogSlide, blogIntervalTime);
+//blogInterval = setInterval(nextBlogSlide, blogIntervalTime);
+
+//Слайдер для touch-событий
+const blogTouchSlider = function(element) {
+  let surface = element;//переменная, в которой хранится область взаимодействий с пользователем
+  let startX = 0;//стартовая позиция по оси х для курсора
+  let startY = 0;
+  let distanceX = 0;//пройденная дистанция по оси х
+  let distanceY = 0;
+  let threshold = 100;//min дистанция, при котором сработает свайп
+  let restraint = 100;//ограничение действия по оси У, max расстояние
+  let allowedTime = 300;//min время, которое должен длиться свайп, чтобы он сработал
+
+  let startTime = 0;
+  let elapsedTime = 0;
+
+  surface.addEventListener("touchstart", function(e) {
+    //Чтобы стрелки навигации работали на мобильных телефонах, планшетах
+    //Содержит ли стрелка навигации один из указанных классов
+    if (e.target.classList.contains("control")) {
+      //Проверка, какая именено стрелка нажимается - левая или правая
+        if (e.target.classList.contains("left")) {
+          prevBlogSlide();
+        } else if (e.target.classList.contains("right")) {
+          nextBlogSlide();
+        }
+    }
+
+    //Переменная для хранения pageX, pageY
+    let touchObj = e.changedTouches[0];
+    startX = touchObj.pageX;
+    startY = touchObj.pageY;
+    startTime = new Date().getTime();
+    //Блокировка других событий
+    e.preventDefault();
+  });
+
+  surface.addEventListener("touchmove", function(e) {
+    //Блокировка других событий
+    e.preventDefault();
+  });
+
+  surface.addEventListener("touchend", function(e) {
+    //Переменная для хранения pageX, pageY
+    let touchObj = e.changedTouches[0];
+    distanceX = touchObj.pageX - startX;
+    distanceY =  touchObj.pageY - startY;
+    //Разность по времени между тем, как пользователь нажал и отпустил
+    elapsedTime = new Date().getTime() - startTime;
+
+    //Проверка условий для свайпа
+    //Если наше время превышает время свайпа, то свайпа быть не должно
+    if (elapsedTime <= allowedTime) {
+      //Для распознавания события как свайпа
+      //Math.abs чтобы избежать ошибки при свайпе влево и вправо
+        if (Math.abs(distanceX) >= threshold && Math.abs(distanceY) <= restraint) {
+            if (distanceX > 0) {
+              prevBlogSlide();
+            } else {
+              nextBlogSlide();
+            }
+        }
+    }
+
+    //Блокировка других событий
+    e.preventDefault();
+  });
+}
+
+blogTouchSlider(blogSlide);
