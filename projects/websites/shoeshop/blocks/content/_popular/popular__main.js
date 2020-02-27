6e1bfb617d92bf1e7e471421ded7508f89c10373
Alexej <nikhaienko@bk.ru>
2020-02-27 11:25:25 +0700

    Shoeshop Для блока "popular" изменил скрипт для слайдера, а также соответствующие стили
    Проблема: при достижении отрицательного значения переменной "substr" слайдер останравливался и не показывал оставшиеся слайды. Поэтому внёс следующие изменения:
      - назначил размеры слайдов и внешний правый отступ ч/з JS. В массив "slidesWidthArr" внёс их общую ширину (чтобы в дальнейших расчётах не писать лишний код). Отказался от назначения ширины слайдов ч/з метод "offsetWidth", т.к. он давал погрешность и слайд при прокрутке скрывался частично, а не полностью;
    
      - общую ширину обёртки для слайдов определил на основании пункта выше;
      - переписал функцию "shiftSliderToLeft", в частности:
          - при вычислении переменной "substr" добавил значения из массива "slidesWidthArr", т.к. до этого расчёты были не  корректными;
          - в "else" добавлил условия для работы в случае, когда substr < 0, обнулил смещение, текущему индексу присвоил значение "-1", чтобы не учитывать начальное значение в массиве "slidesWidthArr" (равняется 0)

--- projects/websites/shoeshop/blocks/content/_popular/popular__main.js
+++ projects/websites/shoeshop/blocks/content/_popular/popular__main.js
@@ -2,41 +2,56 @@ const popularBlockWidth = document.querySelector(".popular").offsetWidth;
 const popSlider = document.querySelector(".popular__wrap");
 const popSlidesArray = document.querySelectorAll(".product");
 
-let shiftSlidesToLeft = 0;
-let shiftSlidesToRight = 0;
-let curIndexPopSlide = 0;
-const slidesWidthArray = [0];
-let popSliderWidth = 0;
-const slidesMarginRight = 10;
+let shiftToLeft = 0;//смещение влево
+let curIndex = 0;//индекс текущего слайда
+let slidesWidthArr = [0];//массив с "ширина слайда + отступ"
+let sliderWidth = 0;//ширина обёртки для слайдов
 
+let marginRight = popularBlockWidth * 0.025;//правый внешний отступ слайда (marginRight)(согласно макету равен 2,5%)
+let items = 4;//колличество одновременно выводимых слайдов (согласно макета)
+let widthSlide = (popularBlockWidth - (popularBlockWidth * 0.025 * (items - 1))) / items;//ширина отдельного слайда
+//console.log(widthSlide);
+
+//console.log(popularBlockWidth);
+//Цикл для установки всех необходимых метрик для слайдера
 for (let i = 0; i < popSlidesArray.length; i++) {
-  slidesWidthArray.push(popSlidesArray[i].offsetWidth);
-
+  //назначение для слайдов внених отступов
   if (i == popSlidesArray.length - 1) {
-    popSlidesArray[i].style.marginRight = 0;
+    marginRight = 0;
+    popSlidesArray[i].style.marginRight = marginRight;
   } else {
-    popSlidesArray[i].style.marginRight = slidesMarginRight + "px"; 
+    popSlidesArray[i].style.marginRight = marginRight + "px";
   }
+  //назначение одинаковой ширины для каждого слайда в массиве 
+  popSlidesArray[i].style.width = widthSlide + "px";
 
-  popSliderWidth += (popSlidesArray[i].offsetWidth + slidesMarginRight);
+  slidesWidthArr.push(widthSlide + marginRight);//добавление в массив размеров сладов
+  sliderWidth += widthSlide + marginRight;//определение итоговой ширины обёртки для слайдов
 }
 
-const substraction = popSliderWidth - popularBlockWidth;
+//назначение ширины обёртки для слайдов
+popSlider.style.width = sliderWidth + "px";
+/*console.log(popSlider.style.width);
+console.log("sliderWidth = " + sliderWidth);
+console.log(slidesWidthArr);*/
 
+//функция для смещения слайдов влево
 function shiftSliderToLeft() {
-  shiftSlidesToLeft += slidesWidthArray[curIndexPopSlide];
-  curIndexPopSlide++;
-  //let substraction = popSliderWidth - popularBlockWidth - shiftSlidesToLeft;
-  if (shiftSlidesToLeft < substraction) {
-    popSlider.style.left = 0;
+  let substr = sliderWidth - popularBlockWidth - (shiftToLeft + slidesWidthArr[curIndex]);
+  if (substr >= 0) {
+    shiftToLeft += slidesWidthArr[curIndex];
+    popSlider.style.left = -shiftToLeft + "px";
   } else {
-    popSlider.style.left = -shiftSlidesToLeft + "px";
+    popSlider.style.left = -(sliderWidth - popularBlockWidth) + "px";
+    shiftToLeft = 0;
+    curIndex = -1;
   }
-  console.log(shiftSlidesToLeft);
+  curIndex++;
+  console.log(curIndex, substr, shiftToLeft, popSlider.style.left);
 }
-
 shiftSliderToLeft();
 
+//функция для запуска автослайдера
 function autoSlider() {
   setInterval(shiftSliderToLeft, 1500);
 }
@@ -52,53 +67,57 @@ function autoSlider() {
 
 
 
+/*let shiftToLeft = 0;
+let curIndex = 0;
+let slidesWidthArr = [0];
+let sliderWidth = 0;
+let marginRight = popularBlockWidth * 0.025;
+//let extraSize = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const widthSlidesArr = [0];
-const slidesMarginRight = 10;
-let curIndexPopSlide = 0;
-let shift = 0;//смещение
-
-let timer;
-let time = 1500;
+console.log(popularBlockWidth);
 
 for (let i = 0; i < popSlidesArray.length; i++) {
-  widthSlidesArr.push(popSlidesArray[i].offsetWidth);
-
   if (i == popSlidesArray.length - 1) {
-    popSlidesArray[i].style.marginRight = 0;
+    marginRight = 0;
+    popSlidesArray[i].style.marginRight = marginRight;
   } else {
-    popSlidesArray[i].style.marginRight = slidesMarginRight + "px";
+    popSlidesArray[i].style.marginRight = marginRight + "px";
   }
-}
 
-function shiftPopSlide() {
-  shift += widthSlidesArr[curIndexPopSlide];
-  curIndexPopSlide++;
-  popSlider.style.left = -shift + "px";
-
-  if (curIndexPopSlide == widthSlidesArr.length - 1) {
-    clearInterval(timer);
-  }
-  console.log("popSlider.style.left[" + curIndexPopSlide + "] = " + (shift + widthSlidesArr[curIndexPopSlide]));
+  slidesWidthArr.push(popSlidesArray[i].offsetWidth + marginRight);
+  sliderWidth += popSlidesArray[i].offsetWidth + marginRight;
 }
-shiftPopSlide();
+console.log("sliderWidth = " + sliderWidth);
+console.log(slidesWidthArr);
 
-function autoSlide() {
-  timer = setInterval(shiftPopSlide, time);
+function shiftSliderToLeft() {
+  let substr = sliderWidth - popularBlockWidth - (shiftToLeft + slidesWidthArr[curIndex]);
+  //let substr = sliderWidth - popularBlockWidth - shiftToLeft;
+  //extraSize += 0.1656;
+  if (substr >= 0) {
+    shiftToLeft += slidesWidthArr[curIndex];
+    //extraSize += 0.1656;
+    //popSlider.style.left = -shiftToLeft - (slidesWidthArr[curIndex] * extraSize) + "px";
+    //popSlider.style.left = -(shiftToLeft + marginRight) + "px";
+    popSlider.style.left = -shiftToLeft + "px";
+    //curIndex++;
+  } else {
+    popSlider.style.left = -(sliderWidth - popularBlockWidth) + "px";
+    shiftToLeft = 0;
+    curIndex = -1;
+    /*extraSize += 0.1657;
+    popSlider.style.left = -(sliderWidth - popularBlockWidth + (slidesWidthArr[curIndex] * extraSize)) + "px";*/
+    //popSlider.style.left = -(sliderWidth - popularBlockWidth + (popularBlockWidth * marginRight * popSlidesArray.length)) + "px";
+   // curIndex--;
+   //console.log(marginRight);
+   //clearInterval(autoSlider);
+  /*}
+  curIndex++;
+  console.log(curIndex, substr, shiftToLeft, popSlider.style.left);
 }
-//autoSlide();*/
+shiftSliderToLeft();
+
+function autoSlider() {
+  setInterval(shiftSliderToLeft, 1500);
+}
+//autoSlider();*/
