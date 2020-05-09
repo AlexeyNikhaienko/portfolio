889640088d57aba2110091724d9fda162e1e42e8
Aleksej <nikhaienko@bk.ru>
2020-05-09 17:24:45 +0700

    IMWOMAN Блок "header": в скрипт добавил функционал для кнопки "shop"
    При нажатии на кнопку "shop" выполняется переадресация на форму регистрации

--- projects/websites/iamwoman/blocks/header/loginForm.js
+++ projects/websites/iamwoman/blocks/header/loginForm.js
@@ -1,9 +1,16 @@
 //Объявление переменных
 const btnVisibleForm= document.querySelector(".header .user");//Кнопка для показа регистрационной формы
 const btnHideForm = document.querySelector(".header .close-form");//Кнопка для скрытия регистрационной формы
+//Кнопка для показа корзины покупок. По умолчанию переводит на форму регистрации
+const btnShop = document.querySelector(".header .shop");
 
 /*Установка обработчиков событий на кнопки*/
 btnVisibleForm.addEventListener("click", () => {
+      /*Установка стилей*/
+      document.querySelector(".modal").setAttribute("style", "transform: translateX(-100%)");
+});
+
+btnShop.addEventListener("click", () => {
       /*Установка стилей*/
       document.querySelector(".modal").setAttribute("style", "transform: translateX(-100%)");
 });
