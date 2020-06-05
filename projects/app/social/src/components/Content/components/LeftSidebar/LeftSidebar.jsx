36ef533f1cb63cd6b64ba8f0c42a2fbf39f8da99
Aleksej <nikhaienko@bk.ru>
2020-06-05 11:52:27 +0700

    NETWORKIA (вёрстка): Внёс измения в стили компонентов Content, LeftSidebar, а также в сам LeftSidebar
    Изменения:
        - свойство align-items перенёс из стилей компонета Content в стили компонента LeftSidebar (присвоил его классу "aside"). Это связано с тем, на всю высоту подителя растягивался ТОЛЬКО центральный элемент с контентом, а боковые панели занимали лишь необходимую высоту;
        - LeftSidebar через пропсы передаёт фоновое изображение, а также дополнительные стили для аватарки и компонента ProfileOwner в компонент ProfileOwner. Это решение вызвано тем, что стили для данных компонентов изменяются в зависимости от того, в каком блоке расположен компонент ProfileOwner

--- projects/app/social/src/components/Content/components/LeftSidebar/LeftSidebar.jsx
+++ projects/app/social/src/components/Content/components/LeftSidebar/LeftSidebar.jsx
@@ -3,16 +3,36 @@ import React from 'react';
 
 //Импорт стилей
 import './css/LeftSidebar.css';
+import bcg from '../../../../common_files/components/ProfileOwner/img/bcg1.jpg'
 
 //Импорт компонентов
 import ProfileOwner from '../../../../common_files/components/ProfileOwner/ProfileOwner';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import FriendList from './components/FriendList/FriendList';
 
+//Дополнительные стили/классы, применимые ТОЛЬКО в рамках компонента LeftSidebar:
+//для аватарки
+const extraStylesAvatar = {
+  top: '-50%',
+  transform: 'translateY(25%)'
+}
+
+//для компонента ProfileOwner:
+const extraClassName = 'cover';
+const extraStylesBlock = {
+  backgroundImage: `url(${bcg})`,
+  borderRadius: '8px',
+  boxShadow: '0 0.3125em 0.625em #999999'
+}
+
 function LeftSidebar(props) {
   return (
-    <aside className="aside aside--bcg__transparent">
-      <ProfileOwner>
+    <aside className="content__aside aside aside--bcg__transparent">
+      <ProfileOwner 
+          extraStylesAvatar={extraStylesAvatar}
+          extraClassName={extraClassName}
+          extraStylesBlock={extraStylesBlock}
+      >
         <section className="profileOwner__ownerInfo ownerInfo">
           <h2 className="ownerInfo__ownerName ownerName">User-1</h2>
           <p className="ownerInfo__totatFollowers totatFollowers">1370</p>
